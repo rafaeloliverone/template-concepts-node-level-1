238de1a86b991dd14c3bd4ef69d3ea5ce1557916
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-07-14 02:48:59 -0300

    all completed methods passing the test

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
+
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
@@ -30,17 +31,57 @@ app.post("/repositories", (request, response) => {
 
 });
 
+
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Not found repository with this ID'});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  return response.status(200).json(repository);
 });
+
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Not found repository with this ID'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
+
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Not found repository with this ID'});
+  }
+
+  repositories[repositoryIndex].likes += 1; 
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
